93c266d855d8da0c90aece68bf3cab826ed52f50
IliDevia <neiluamaibi@mail.ru>
2023-04-17 17:23:56 +0300

    StaticClassMethod

--- lib.py
+++ lib.py
@@ -1,8 +1,18 @@
 class Point:
+    __minCoor = 0
+    __maxCoor = 1000
+
+    @classmethod
+    def test(cls, x, y):
+        return cls.__minCoor <= x <= cls.__maxCoor and cls.__minCoor <= y <= cls.__maxCoor
+
     def __init__(self, x, y):
-        self.__x = x
-        self.__y = y
-        print(f'created: {self}')
+        if self.test(x, y):
+            self.__x = x
+            self.__y = y
+            print(f'created: {self}')
+        else:
+            raise Exception('no')
 
     def getCoor(self):
         return self.__x, self.__y
@@ -13,3 +23,7 @@ class Point:
 
     def __del__(self):
         print(f'deleted: {self}')
+
+    @staticmethod
+    def gip(a, b):
+        return (a ** 2 + b ** 2) ** 0.5
--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 from lib import Point
 
 
-a = Point(10, 20)
+a = Point(10000, 20)
 b = Point(100, 200)
+g = Point.gip(3, 4)
+print(g)
+
+Point.test(100, 200)

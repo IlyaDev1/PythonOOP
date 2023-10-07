b3c32667e0b1b0140f9d97a1dd77550c0a72485b
IliDevia <neiluamaibi@mail.ru>
2023-10-07 15:14:13 +0300

    GetSet4

--- lib.py
+++ lib.py
@@ -1,13 +1,17 @@
 class Point:
-    __x = 0
+    x = 0
     __y = 0
     __MinCoor = 0
     __MaxCoor = 10
 
+    @classmethod
+    def testCoor(cls, value):
+        return cls.__MinCoor <= value <= cls.__MaxCoor
+
     def __init__(self, x=0, y=0):
         self.__x = x
         self.__y = y
-        print(self)
+        print(f'object was created: {self}')
 
     def getCoor(self):
         return self.__x, self.__y
@@ -16,5 +20,23 @@ class Point:
         self.__x = x
         self.__y = y
 
+    def __getattribute__(self, item):  # вызывается, если мы обращаемся к переменной
+        if item == 'x' or item == '__x' or item == 'y' or item == '__y':
+            raise Exception('нельзя юзать эту переменную')
+        return object.__getattribute__(self, item)
+
+    def __setattr__(self, key, value):  # вызывается, если мы редачим переменную
+        if key == '_Point__x' or key == '_Point__y':
+            if not self.testCoor(value):
+                raise Exception('Вы вводите неверные значения')
+        object.__setattr__(self, key, value)
+
+    def __getattr__(self, item):  # вызывается при обращении к несущ атр
+        return 'вы исп несущ атр'
+
+    def __delattr__(self, item):
+        print(f'был удален атр {item}')
+        object.__delattr__(self, item)
+
     def __del__(self):
-        print(self)
+        print(f'object was deleted: {self}')
--- main.py
+++ main.py
@@ -3,3 +3,5 @@ from lib import Point
 
 a = Point()
 b = Point(1, 2)
+
+

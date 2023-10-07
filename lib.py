10e6a63810bd86c6a023f50da787c00a5de5982d
IliDevia <neiluamaibi@mail.ru>
2023-10-07 14:48:32 +0300

    ClsStatMethod

--- lib.py
+++ lib.py
@@ -1,13 +1,23 @@
+from math import sqrt
+
+
 class Point:
     __x = 0
     __y = 0
     __MinCoor = 0
     __MaxCoor = 10
 
+    @classmethod
+    def test(cls, num):
+        return cls.__MinCoor <= num <= cls.__MaxCoor
+
     def __init__(self, x=0, y=0):
-        self.__x = x
-        self.__y = y
-        print(self)
+        if self.test(x) and self.test(y):
+            self.__x = x
+            self.__y = y
+            print(self)
+        else:
+            raise(Exception('x, y range'))
 
     def getCoor(self):
         return self.__x, self.__y
@@ -15,6 +25,10 @@ class Point:
     def setCoor(self, x, y):
         self.__x = x
         self.__y = y
+
+    @staticmethod
+    def gip(x, y):
+        return sqrt(x ** 2 + y ** 2)
 
     def __del__(self):
         print(self)

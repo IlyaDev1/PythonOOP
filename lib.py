392a7f4908b22d4b36a55c79d2dbe6111ff3e08e
IliDevia <neiluamaibi@mail.ru>
2023-12-09 12:56:52 +0300

    Add

--- lib.py
+++ lib.py
@@ -9,3 +9,17 @@ class Complex:
 
     def __str__(self):
         return f'{self.__x} + {self.__y}i'
+
+    def __add__(self, other):
+        if isinstance(other, Complex):
+            x = self.__x + other.__x
+            y = self.__y + other.__y
+            return Complex(x, y)
+
+        elif isinstance(other, str):
+            if other[-1] == 'i':
+                y = self.__y + int(other.split('i')[0])
+                return Complex(self.__x, y)
+            else:
+                x = self.__x + int(other)
+                return Complex(x, self.__y)
--- main.py
+++ main.py
@@ -2,6 +2,9 @@ from lib import Complex
 
 
 n1 = Complex(-2, 3)
+n1 = n1 + '5'
+n1 = n1 + '6i'
 print(n1)
-abs(n1)
-print(n1)
+n2 = Complex(10, 10)
+n3 = n1 + n2
+print(n3)

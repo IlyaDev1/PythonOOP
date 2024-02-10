570343153946f4cdb99ef7941e0af8f7a8c542f5
IliDevia <neiluamaibi@mail.ru>
2024-02-11 00:49:48 +0300

    TaskInh1BankAccount

--- lib.py
+++ lib.py
@@ -1,9 +1,31 @@
-class Test:
-    x = 5
+class BankAccount:
+    def __init__(self, login, pwd, cash=0):
+        self._login = login
+        self._pwd = pwd
+        self._cash = cash
 
-    def check(self):
-        print('hi')
+    def __str__(self):
+        return f'Сумма на счете {self._login}: {self._cash}'
+
+    def __add__(self, other):
+        return BankAccount(self._login, self._pwd, self._cash + other)
+
+    def __radd__(self, other):
+        return self + other
+
+    def __iadd__(self, other):
+        return self + other
+
+    def __sub__(self, other):
+        return BankAccount(self._login, self._pwd, self._cash - other)
+
+    def __rsub__(self, other):
+        return self - other
+
+    def __isub__(self, other):
+        return self - other
 
 
-class TestInh(Test):
-    pass
+class SavingAccount(BankAccount):
+    def chargePercent(self):
+        self._cash = (self._cash * 1.07 - self._cash) / 12 + self._cash
--- main.py
+++ main.py
@@ -1,7 +1,14 @@
-from lib import TestInh
+from datetime import datetime
+from lib import SavingAccount
 
 
-a = TestInh()
 
-a.check()
-print(a.x)
+myTime = datetime.now()
+flag = 0
+ac1 = SavingAccount('ilya1', '123', 100)
+while True:
+    check = float(str(datetime.now() - myTime)[5:])
+    if int(check) % 1 == 0 and flag != int(check):
+        flag = int(check)
+        ac1.chargePercent()
+        print(ac1)

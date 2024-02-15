99e539f6624db5d9d0e7386fe75a806a7bb9c9e0
IliDevia <neiluamaibi@mail.ru>
2024-02-15 14:14:53 +0300

    ComplexFullProject

--- lib.py
+++ lib.py
@@ -25,4 +25,49 @@ class Complex:
 
     def __iadd__(self, other):
         return self + other
-    # __sub__, __mul__, __truediv__, __floordiv__, __mod__
+
+    def __sub__(self, other):
+        if isinstance(other, Complex):
+            return Complex(self.__r - other.__r, self.__i - other.__i)
+        elif isinstance(other, int) or isinstance(other, float):
+            return Complex(self.__r - other, self.__i)
+        else:
+            raise TypeError('not correct other data')
+
+    def __isub__(self, other):
+        return self - other
+
+    def __rsub__(self, other):
+        return self - other
+
+    def __mul__(self, other):
+        if type(other) in [int, float]:  # здесь представлена более "красивая" запись
+            other = Complex(other, 0)
+        if isinstance(other, Complex):
+            r = self.__r*other.__r - self.__i*other.__i
+            i = self.__r*other.__i + self.__i*other.__r
+            return Complex(r, i)
+        else:
+            raise TypeError('not correct other data')
+
+    def __rmul__(self, other):
+        return self * other
+
+    def __imul__(self, other):
+        return self * other
+
+    def __truediv__(self, other):
+        if type(other) in [int, float]:
+            other = Complex(other, 0)
+        if isinstance(other, Complex):
+            r = (self.__r*other.__r + self.__i*other.__i) / (other.__r**2+other.__i**2)
+            i = (other.__r*self.__i - self.__r*other.__i) / (other.__r**2+other.__i**2)
+            return Complex(r, i)
+        else:
+            raise TypeError('not correct other data')
+
+    def __rtruediv__(self, other):
+        return self / other
+
+    def __itruediv__(self, other):
+        return self / other
--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from lib import Complex
 c1 = Complex(-2, -5)
 c2 = Complex(5, 10)
 
-c3 = c1 + c2
-c4 = 5 + c2 + 6
-c2 += c1
-print(c1, c2, c3, c4, sep='\n')
+print(c1 + c2)
+print(c1 - c2)
+print(c1 * c2)
+print(c1 / c2)

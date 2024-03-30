a09d8575b5eb95ef9d5a057f680eef6a41b0d734
IliDevia <neiluamaibi@mail.ru>
2024-03-30 22:19:07 +0300

    class File

--- lib.py
+++ lib.py
@@ -1,1 +1,33 @@
-#  коммит для примера
+class File:
+    def __init__(self, name):
+        self._name = name
+        self._file = open(self._name, 'a+')
+
+    def __del__(self):
+        self._file.close()
+
+
+class TxtFile(File):
+    def __init__(self, name):
+        if name[name.rfind('.')+1::] == 'txt':
+            if name.count('.') == 1:
+                super().__init__(name)
+            else:
+                raise ValueError('not correct file name')
+        self.content = self.getContent(self._file)
+        self.__ban = '0123456789'
+
+    @classmethod
+    def getContent(cls, file):
+        file.seek(0)
+        s = file.readlines()
+        mas = [el[:-1] for el in s]
+        mas[-1] += s[-1][-1]
+        return mas
+
+    def addString(self, s):
+        for c in self.__ban:
+            if c in s:
+                raise ValueError('not correct symbol in string')
+        self._file.write('\n'+s)
+        self.content.append(s)
--- main.py
+++ main.py
@@ -1,20 +1,7 @@
-from lib import Frac
+from lib import TxtFile
 
 
-a = Frac(2, 5)
-b = Frac(20)
-c = Frac('20/40')
-d = Frac(12.5)
-print(a)
-print(b)
-print(c)
-print(d)
-
-e = Frac(-4, 5)
-print(abs(e))
-
-print(a + b)
-
-a = Frac(10, 20)
-b = Frac(10, 20)
-print(a == b, a == e)
+a = TxtFile('test.txt')
+print(a.content)
+a.addString('hello world')
+print(a.content)
